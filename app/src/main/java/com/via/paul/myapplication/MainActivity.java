d48702a627326f638dcb37cc01aee4abf87fa5b4
paul <paul.lesur@free.fr>
2015-07-30 20:08:13 +0200

    Affichage d'une liste de morceaux et selection du morceau

--- app/src/main/java/com/via/paul/myapplication/MainActivity.java
+++ app/src/main/java/com/via/paul/myapplication/MainActivity.java
@@ -4,13 +4,26 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -18,6 +31,8 @@ public class MainActivity extends ActionBarActivity {
     Button play, stop;
     SeekBar sk;
     MediaPlayer mp;
+    ListView lv;
+    Map<String, File> chansons;
 
     boolean isPlaying;
 
@@ -28,6 +43,9 @@ public class MainActivity extends ActionBarActivity {
 
         isPlaying = false;
 
+        chansons = new HashMap<>();
+
+
         play = (Button)findViewById(R.id.button);
         stop = (Button)findViewById(R.id.button3);
 
@@ -35,33 +53,9 @@ public class MainActivity extends ActionBarActivity {
 
         mp = MediaPlayer.create(getApplicationContext(), Uri.parse("/sdcard/music/play.mp3"));
 
-        play.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(isPlaying){
-                    play.setText("►");
-                    mp.pause();
-                    isPlaying=false;
-                }else {
-                    play.setText("||");
-                    mp.start();
-                    isPlaying=true;
-                }
-            }
-        });
-
-
-
-        stop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mp.seekTo(0);
-                mp.pause();
-                play.setText("►");
-                isPlaying=false;
-            }
-        });
-
+        lv = (ListView)findViewById(R.id.listView);
+        TextView test = new TextView(getApplicationContext());
+        test.setText("element");
 
         sk.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -82,7 +76,9 @@ public class MainActivity extends ActionBarActivity {
 
             }
         });
+
         sk.setMax(mp.getDuration());
+
         sk.postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -90,6 +86,70 @@ public class MainActivity extends ActionBarActivity {
                 sk.postDelayed(this, 100);
             }
         }, 100);
+
+        File dossier = new File(Environment.getExternalStorageDirectory().getPath()+"/music/");
+        int nb = 0;
+        File[] fichiers = dossier.listFiles();
+        for (File fichier: fichiers) {
+            chansons.put(fichier.getName(), fichier);
+            nb++;
+        }
+
+        String[] tabChansons = chansons.keySet().toArray(new String[0]);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, tabChansons);
+
+
+        // Assign adapter to ListView
+        lv.setAdapter(adapter);
+
+
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isPlaying) {
+                    play.setText("►");
+                    mp.pause();
+                    isPlaying = false;
+                } else {
+                    play.setText("||");
+                    mp.start();
+                    isPlaying = true;
+                }
+            }
+        });
+
+
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.seekTo(0);
+                mp.pause();
+                play.setText("►");
+                isPlaying=false;
+            }
+        });
+
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String itemValue = (String) lv.getItemAtPosition(position);
+                mp.stop();
+                mp = MediaPlayer.create(getApplicationContext(), Uri.parse("/sdcard/music/" + itemValue));
+                play.setText("||");
+                mp.start();
+                isPlaying = true;
+                sk.setMax(mp.getDuration());
+                Toast.makeText(getApplicationContext(), itemValue, Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
     }
 
     @Override
@@ -102,5 +162,9 @@ public class MainActivity extends ActionBarActivity {
         NotificationManager nm = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
 
     }
+
+
+
+
 
 }
